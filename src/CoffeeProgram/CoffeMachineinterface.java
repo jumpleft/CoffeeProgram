fb70e949aac56d9c07879e2fc75378f03f1dc131
Matthew7 <matthewneves77@gmail.com>
2020-03-22 20:18:50 -0500

    down to 3 files

--- src/CoffeeProgram/CoffeMachineinterface.java
+++ src/CoffeeProgram/CoffeMachineinterface.java
@@ -5,4 +5,18 @@ public interface CoffeMachineinterface {
 	public void chooseFirstSelection();	
 	public void chooseSecoundSelection();
 	
+	
+	public static void main (String[] arrgs){
+		
+		OldCoffeeMachine ToBeFittedWithTouchScreeen = new OldCoffeeMachine();
+		CoffeeTouchscreenAdapter TouchScreenInterface = new CoffeeTouchscreenAdapter(ToBeFittedWithTouchScreeen);
+		TouchScreenInterface.chooseFirstSelection();
+		TouchScreenInterface.chooseSecoundSelection();
+		
+		
+	}
+	
+	
+	
+	
 }
--- src/CoffeeProgram/Program.java
+++ src/CoffeeProgram/Program.java
@@ -1,18 +1,2 @@
 package CoffeeProgram;
 
-public class Program {
-	
-	public static void main (String[] arrgs){
-		
-		OldCoffeeMachine ToBeFittedWithTouchScreeen = new OldCoffeeMachine();
-		CoffeeTouchscreenAdapter TouchScreenInterface = new CoffeeTouchscreenAdapter(ToBeFittedWithTouchScreeen);
-		TouchScreenInterface.chooseFirstSelection();
-		TouchScreenInterface.chooseSecoundSelection();
-		
-		
-	}
-	
-	
-	
-	
-}
